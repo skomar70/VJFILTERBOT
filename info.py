8ea1ef05818bf4aa29ade45f0a7042f1ddf57b82
skomar70 <mosharofroydev@gmail.com>
2025-10-03 11:25:55 +0000

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ id_pattern = re.compile(r'^[+-]?\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24776633'))
-API_HASH = environ.get('API_HASH', '57b1f632044b4e718f5dce004a988d69'')
+API_HASH = environ.get('API_HASH', '57b1f632044b4e718f5dce004a988d69')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8269129164:AAHiGH5Oiv2vJiUO_DUlfxwksxSq4dFtuQ0")
 
 
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
